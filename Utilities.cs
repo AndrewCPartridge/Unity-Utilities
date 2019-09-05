203ac5447e7924660f7c726d5d3cd8f2438b10b4
AndrewCPartridge <46792453+AndrewCPartridge@users.noreply.github.com>
2019-09-05 11:20:45 -0600

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities
+{
+
+    public static string ProcessText(string input)
+    {
+        // extra cred
+        
+        //code goes here
+        //determine whether user has a word or a number
+        //return 'word' if word (includes ab.23cd)
+        //return"number" if number(includes 1.3) etc
+        float num;
+
+        if(float.TryParse(input, out num))
+        {
+            input = "number";
+            
+        }
+        else
+        {
+            input = "word";
+        }
+
+        
+        return input;
+    }
+}
+
